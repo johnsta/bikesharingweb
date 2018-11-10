c67aa6c07349dc5da6d1a8951c22cfc1eaeeb0cc
John Stallo <john.stallo@live.com>
2018-11-09 17:30:51 -0800

    list bikes

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import Page from "../components/Page";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Content from "../components/Content";
-import Link from 'next/link';
+import React, { Component } from 'react'
+import Page from "../components/Page"
+import Header from "../components/Header"
+import Content from "../components/Content"
+import Link from 'next/link'
+import BikeCard from "../components/BikeCard"
 
 export default class extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bikes: [
+                { id: "1", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-1.png" },
+                { id: "2", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-2.png" },
+                { id: "3", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-3.png" },
+                { id: "4", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-4.png" },
+                { id: "2", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-2.png" },
+                { id: "1", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-1.png" },
+                { id: "4", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-4.png" },
+                { id: "1", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-1.png" },
+                { id: "2", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-2.png" },
+                { id: "3", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-3.png" },
+                { id: "4", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-4.png" },
+                { id: "2", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-2.png" },
+                { id: "1", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-1.png" },
+                { id: "4", name: "Schwinn Women's Cruiser", address: "5555 Beeler Street, Pittsburgh, PA 15217", rate: "5", imageUrl: "/static/sample-bike-4.png" },
+            ]
+        }
+    }
+
     render() {
+        var listBikes = this.state.bikes.map(function (bike) {
+            return (
+                <BikeCard id={bike.id} name={bike.name} address={bike.address} rate={bike.rate} imageUrl={bike.imageUrl} />
+            );
+        });
         return (
             <Page>
                 <Header />
                 <Content>
-                    <h2>Home</h2>
-                    
-                    <Link href="/about">
-                        <a>Go to about</a>
-                    </Link>{'  '}
+                    {listBikes}
                 </Content>
             </Page>
         );
